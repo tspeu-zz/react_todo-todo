f5c420c5a646853a2301c7cd3022a59c226a9831
JM_B <jmbanchero@gmail.com>
2019-01-31 16:12:00 +0000

    ...add

---
+++ docs/build/precache-manifest.088bef8da0de06cbe07ff297a9491733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f508c270b0de4f39118e",
+    "url": "/static/js/main.f508c270.chunk.js"
+  },
+  {
+    "revision": "b3b75bb67bef776c7c6d",
+    "url": "/static/js/1.b3b75bb6.chunk.js"
+  },
+  {
+    "revision": "f508c270b0de4f39118e",
+    "url": "/static/css/main.a692d75a.chunk.css"
+  },
+  {
+    "revision": "b3b75bb67bef776c7c6d",
+    "url": "/static/css/1.9e125cc7.chunk.css"
+  },
+  {
+    "revision": "3014d6d1edfbae543fad0e212cb862af",
+    "url": "/index.html"
+  }
+];
