b69a68c0a6014df8c0ed80ca22e0b717e4fd2780
JM_B <jmbanchero@gmail.com>
2019-01-31 13:45:34 +0000

    ...nav

--- src/App.js
+++ src/App.js
@@ -1,64 +1,32 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+// import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 import Todos from './app/Todos';
+import NavBarHeader from './app/layout/navar';
 
 class App extends Component {
 
-  // state = {
-  //   todos : [
-  //     {
-  //       id : 1, 
-  //       title: 'sacar la basura',
-  //       completed: false
-  //     },
-  //     {
-  //       id : 2, 
-  //       title: 'cenar con la flia.',
-  //       completed: false
-  //     },
-  //     {
-  //       id : 3, 
-  //       title: 'pasear al perro',
-  //       completed: false
-  //     }
-  //   ]
-  // };
+  state = {
+    todos : [
+      {
+        id : 1, 
+        title: 'sacar la basura',
+        completed: false
+      },
+      {
+        id : 2, 
+        title: 'cenar con la flia.',
+        completed: true
+      },
+      {
+        id : 3, 
+        title: 'pasear al perro',
+        completed: false
+      }
+    ]
+  };
 
-
-  constructor(props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-
-    this.state = {
-      collapsed: true,
-      todos : [
-        {
-          id : 1, 
-          title: 'sacar la basura',
-          completed: false
-        },
-        {
-          id : 2, 
-          title: 'cenar con la flia.',
-          completed: true
-        },
-        {
-          id : 3, 
-          title: 'pasear al perro',
-          completed: false
-        }
-      ]
-    };
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
 
   //@ Recive desde ITemTodos y cambia el estado de markComplete
   checkCompleteTask = (id) => {
@@ -94,39 +62,22 @@ class App extends Component {
 
     return (
 
-    <div className="App">
+      <div className="App">
+        
+          < NavBarHeader />
       
-      <Navbar color="dark" dark>
-
-        <NavbarBrand href="/" className="mr-auto">
-          <img src={logo}  alt="logo"/>Todo React
-        </NavbarBrand>
-
-        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse isOpen={!this.state.collapsed} navbar>
-            <Nav navbar className="white-text">
-              <NavItem >
-                <NavLink href="/components/" >Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink target="_blank" href="https://github.com/tspeu">GitHub</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-      </Navbar>
-    
-        <div className="jumbotron">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code  className="App-link">src/App.js</code> and save to reload.
-          </p>
-        </div>
-
-          <div className="container">
-              <Todos todos={this.state.todos} 
-                      taskComplete= {this.checkCompleteTask} 
-                      borraTodo= {this.deleteTodo } />
+          <div className="jumbotron">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code  className="App-link">src/App.js</code> and save to reload.
+            </p>
           </div>
+
+            <div className="container">
+                <Todos todos={this.state.todos} 
+                        taskComplete= {this.checkCompleteTask} 
+                        borraTodo= {this.deleteTodo } />
+            </div>
 
       </div>
     );
---
+++ src/app/layout/navar.js
@@ -0,0 +1,52 @@
+import React , {Component }from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+// import logo from '../../../logo.svg';
+
+
+class NavBarHeader extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        }
+
+    }
+    
+    toggleNavbar() {
+    this.setState({
+        collapsed: !this.state.collapsed
+    });
+    }
+
+
+        render() {
+        return (
+            <Navbar color="dark" dark>
+
+            <NavbarBrand href="/" className="mr-auto">
+                {/* <img src={logo}  alt="logo"/> */}
+                Todo React
+            </NavbarBrand>
+
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+
+            <Collapse isOpen={!this.state.collapsed} navbar>
+
+                <Nav navbar className="white-text">
+                    <NavItem >
+                        <NavLink href="/components/" >Components</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink target="_blank" href="https://github.com/tspeu">GitHub</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default NavBarHeader;
